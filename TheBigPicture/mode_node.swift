54dfff2a09800f5f1babe87b346b67e160cf79c5
mac <rnrudgh@gmail.com>
2016-11-01 15:28:29 +0900

    no message

---
+++ TheBigPicture/mode_node.swift
@@ -0,0 +1,24 @@
+//
+//  mode_node.swift
+//  TheBigPicture
+//
+//  Created by mac on 2016. 11. 1..
+//  Copyright © 2016년 RadioactiveTEA. All rights reserved.
+//
+
+import Foundation
+
+
+class Node {
+    var id:Int
+    var parent_id:Int
+    
+    init(id:Int,parent_id:Int) {
+        self.id = id
+        self.parent_id = parent_id
+    }
+    
+    func addComponent(<#parameters#>) -> Void {
+        
+    }
+}
