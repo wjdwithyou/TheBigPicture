48dd447389298d6a655f3ed51bfb3edb61c9fd43
RadioactiveTEA <radioactivetea@RadioactiveTEAui-MacBook-Pro.local>
2016-11-22 14:42:18 +0900

    for merge Viewcontroller

--- TheBigPicture/ViewController.swift
+++ TheBigPicture/ViewController.swift
@@ -19,6 +19,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func arrange_cascade(pos:CGPoint, node:Node)
+    {
+        var stride_x:CGFloat = 100;
+        var node_height:CGFloat = 20;
+        var child_y:CGFloat = node_height + 5;
+        
+        for child in node.children
+        {
+            var child_pos:CGPoint;
+            
+            child_pos.x = pos.x + stride_x
+            child_pos.y = pos.y + child_y
+            
+            child_y += arrange_cascade(pos:child_pos, node:child);
+        }
+        
+        node.x = pos.x;
+        node.y = pos.y;
+    
+        return child_y;
+    }
 
 
 }
