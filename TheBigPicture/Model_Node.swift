67ea16e06b9f50d0510e9b2e788aa2950f12820c
LoadIdentity <ham8952@gmail.com>
2016-12-06 15:21:28 +0900

    work

--- TheBigPicture/Model_Node.swift
+++ TheBigPicture/Model_Node.swift
@@ -13,6 +13,7 @@ class Model_Node
 {
     var id:Int
     var parent_id:Int
+    var parent:Model_Node?
     var components:Array<Model_Component>
     var children:Array<Model_Node>
     var x:CGFloat
@@ -22,23 +23,24 @@ class Model_Node
     {
         self.id = id
         self.parent_id = parent_id
+        self.parent = nil
         self.components = []
         self.children = []
         self.x = 0
         self.y = 0
     }
     
-    func addChild(child:Model_Node) -> Void
+    func add_child(child:Model_Node) -> Void
     {
         children.append(child)
     }
     
-    func addComponent(element:Model_Component) -> Void
+    func add_component(component:Model_Component) -> Void
     {
-        components.append(element)
+        components.append(component)
     }
     
-    func removeComponent(element:Model_Component) -> Void
+    func remove_component(component:Model_Component) -> Void
     {
     }
 }
@@ -63,6 +65,70 @@ class Model_NodeContainer
             self.root_node = node
         }
     
-        self.map_node[node_id] = node;
+        self.map_node[node_id] = node
+    }
+    
+    func get_node(node_id:Int) -> Model_Node
+    {
+        return self.map_node[node_id]!
+    }
+}
+
+var s_node_container = Model_NodeContainer()
+
+var test_nodes = [
+    Model_Node(id:1,    parent_id:-1),
+    Model_Node(id:2,    parent_id:1),
+    Model_Node(id:21,   parent_id:2),
+    Model_Node(id:22,   parent_id:2),
+    Model_Node(id:23,   parent_id:2),
+    Model_Node(id:3,    parent_id:1),
+    Model_Node(id:31,   parent_id:3),
+    Model_Node(id:4,    parent_id:1),
+    Model_Node(id:41,   parent_id:4),
+    Model_Node(id:42,   parent_id:4),
+    Model_Node(id:5,    parent_id:1),
+    Model_Node(id:6,    parent_id:1),
+    Model_Node(id:7,    parent_id:1)
+]
+
+var test_components_name = [
+    Model_Component_Name(id:1, node_id:1, name:"한양대학교"),
+    Model_Component_Name(id:2, node_id:2, name:"데이터베이스"),
+    Model_Component_Name(id:3, node_id:21, name:"과제"),
+    Model_Component_Name(id:4, node_id:22, name:"데이터베이스 생성"),
+    Model_Component_Name(id:5, node_id:23, name:"쿼리문 작성"),
+    Model_Component_Name(id:6, node_id:3, name:"인공지능"),
+    Model_Component_Name(id:7, node_id:31, name:"시험"),
+    Model_Component_Name(id:8, node_id:4, name:"융합소프트웨어프로젝트"),
+    Model_Component_Name(id:9, node_id:41, name:"과제"),
+    Model_Component_Name(id:10, node_id:42, name:"동영상촬영"),
+    Model_Component_Name(id:11, node_id:5, name:"테스트"),
+    Model_Component_Name(id:12, node_id:6, name:"테스트"),
+    Model_Component_Name(id:13, node_id:7, name:"테스트")
+]
+
+func initialize_test_data()
+{
+    for node in test_nodes
+    {
+        s_node_container.create_node(node_id:node.id, parent_id:node.parent_id)
+    }
+    
+    for var node in test_nodes
+    {
+        if node.parent_id != -1
+        {
+            var parent_node = s_node_container.get_node(node_id:node.parent_id)
+            
+            node.parent = parent_node
+            
+            parent_node.add_child(child:node)
+        }
+    }
+    
+    for comp in test_components_name
+    {
+        s_node_container.get_node(node_id:comp.node_id).add_component(component:comp)
     }
 }
--- TheBigPicture/View_Cascade.swift
+++ TheBigPicture/View_Cascade.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 
-class View_Cascade: UIView {
-    var node_container:Model_NodeContainer
-
-    override init(frame: CGRect) {
-        self.node_container = Model_NodeContainer()
+class View_Cascade: UIView
+{
+    var map_view_node:[Int:View_CascadeNode]
+    
+    override init(frame: CGRect)
+    {
+        self.map_view_node = [:]
         super.init(frame:frame)
         self.initialize()
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        self.node_container = Model_NodeContainer()
+    required init?(coder aDecoder: NSCoder)
+    {
+        self.map_view_node = [:]
         super.init(coder: aDecoder)
         self.initialize()
     }
@@ -27,39 +30,36 @@ class View_Cascade: UIView {
     {
         self.backgroundColor = UIColor(white:0.9, alpha: 1)
         
-        self.add_button(x:20, y:30,  text:"한양대학교")
-        self.add_button(x:60, y:80,  text:"데이터베이스")
-        self.add_button(x:100, y:130,  text:"과제")
-        self.add_button(x:140, y:180, text:"데이터베이스 생성")
-        self.add_button(x:140, y:230, text:"쿼리문 작성")
-        self.add_button(x:60, y:280, text:"인공지능")
-        self.add_button(x:100, y:330, text:"시험")
-        self.add_button(x:60, y:380, text:"융합소프트웨어프로젝트")
-        self.add_button(x:100, y:430, text:"과제")
-        self.add_button(x:140, y:480, text:"동영상촬영")
+        self.render()
     }
     
-    func add_button(x:CGFloat, y:CGFloat, text:String)
+    func render()
     {
-        let btn = UIButton()
+        self.arrange_cascade(pos: CGPoint(x:10, y:30), node: s_node_container.root_node!)
         
-        btn.frame.origin.x = x
-        btn.frame.origin.y = y
-        btn.frame.size.width = 200
-        btn.frame.size.height = 40
-        btn.layer.cornerRadius = 10
-        
-        btn.backgroundColor = UIColor(red: 0.21, green: 0.37, blue: 0.45, alpha: 1)
-        
-        btn.setTitle(text, for: UIControlState.normal)
-        self.addSubview(btn)
+        for node in s_node_container.map_node
+        {
+            if self.map_view_node[node.value.id] == nil
+            {
+                let new_view_node = View_CascadeNode(model:node.value)
+                
+                self.map_view_node[node.value.id] = new_view_node
+                self.addSubview(new_view_node.button)
+            }
+            else
+            {
+                self.map_view_node[node.value.id]?.model = node.value
+            }
+            
+            self.map_view_node[node.value.id]?.render()
+        }
     }
     
     func arrange_cascade(pos:CGPoint, node:Model_Node) -> CGFloat
     {
-        let stride_x:CGFloat = 100
-        let node_height:CGFloat = 20
-        var child_y:CGFloat = node_height + 5
+        let stride_x:CGFloat = 50
+        let node_height:CGFloat = 40
+        var child_y:CGFloat = node_height + 10
         
         for child in node.children
         {
@@ -68,7 +68,7 @@ class View_Cascade: UIView {
             child_pos.x = pos.x + stride_x
             child_pos.y = pos.y + child_y
             
-            child_y += self.arrange_cascade(pos:child_pos, node:child)
+            child_y += self.arrange_cascade(pos:child_pos, node:s_node_container.get_node(node_id:child.id))
         }
         
         node.x = pos.x
