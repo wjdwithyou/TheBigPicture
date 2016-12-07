8d0b04286ab8c3d260a1dc37f7d7db4bbfdfac70
mac <rnrudgh@gmail.com>
2016-12-07 19:42:24 +0900

    calendar work!!

---
+++ TheBigPicture/CellView.swift
@@ -0,0 +1,15 @@
+//
+//  CellView.swift
+//  TheBigPicture
+//
+//  Created by mac on 2016. 12. 7..
+//  Copyright © 2016년 RadioactiveTEA. All rights reserved.
+//
+
+import Foundation
+import JTAppleCalendar
+
+class CellView: JTAppleDayCellView {
+    @IBOutlet var dayLabel: UILabel!
+    
+}
--- TheBigPicture/Controller_Calendar.swift
+++ TheBigPicture/Controller_Calendar.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
-class Controller_Calendar: UIViewController
+class Controller_Calendar: UIViewController,JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate
 {
+    @IBOutlet var calendarView: JTAppleCalendarView!
     required init?(coder aDecoder: NSCoder)
     {
         super.init(coder: aDecoder)
@@ -23,6 +25,40 @@ class Controller_Calendar: UIViewController
     override func viewDidLoad()
     {
         self.view.backgroundColor = UIColor(white:0.9, alpha: 1)
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        calendarView.registerCellViewXib(file: "CellView")
     }
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2016 02 01")! // You can use date generated from a formatter
+        let endDate = Date()                                // You can also use dates created from this function
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 6, // Only 1, 2, 3, & 6 are allowed
+            calendar: Calendar.current,
+            generateInDates: .forAllMonths,
+            generateOutDates: .tillEndOfGrid,
+            firstDayOfWeek: .sunday)
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
+        let myCustomCell = cell as! CellView
+        
+//        // Setup Cell text
+        myCustomCell.dayLabel.text = cellState.text
+        
+        // Setup text color
+        if cellState.dateBelongsTo == .thisMonth {
+            myCustomCell.dayLabel.textColor = UIColor.black
+        } else {
+            myCustomCell.dayLabel.textColor = UIColor.gray
+        }
+    }
+    
 }
 
