b4b715a14a8dca8f77c34380ae6fbfd921399e12
wjdwithyou <asdf142857@gmail.com>
2016-12-12 15:43:48 +0900

    date component?

--- TheBigPicture/Controller_Detail.swift
+++ TheBigPicture/Controller_Detail.swift
@@ -13,7 +13,6 @@ class Controller_Detail: UIViewController
     @IBOutlet weak var DetailTitle: UINavigationItem!
     
     var model: Model_Node!
-    
     var map_view:[View_Component]
     
     required init?(coder aDecoder: NSCoder)
@@ -43,27 +42,37 @@ class Controller_Detail: UIViewController
             
             if let comp_text = comp as? Model_Component_Text
             {
-                self.add_view_comp_text(model:comp_text)
+                self.add_view_comp_text(model: comp_text)
+            }
+            
+            if let comp_date = comp as? Model_Component_Date
+            {
+                self.add_view_comp_date(model: comp_date)
             }
         }
         
         let fab = KCFloatingActionButton()
         fab.plusColor = UIColor(red: 30/255.0, green: 45/255.0, blue: 66/255.0, alpha: 1)
         fab.buttonColor = UIColor(white: 0.9, alpha: 1)
+        fab.addItem(title: "Text", handler:
+            {
+                item in
+                let model_comp_text = Model_Component_Text(id: s_generic_index, node_id: self.model.id, text: "")
+                self.model.add_component(component: model_comp_text)
+                self.add_view_comp_text(model:model_comp_text)
+                
+                s_generic_index += 1
+                
+                self.render()
+        })
         fab.addItem(title: "Date", handler:
         {
             item in
+            let model_comp_date = Model_Component_Date(id: s_generic_index, node_id: self.model.id, date: NSDate.init(timeIntervalSinceNow: 9*60*60))
+            self.model.add_component(component: model_comp_date)
+            self.add_view_comp_date(model: model_comp_date)
             
-            self.render()
-        })
-        fab.addItem(title: "Text", handler:
-        {
-            item in
-            var model_comp_text = Model_Component_Text(id:s_generic_index, node_id:self.model.id, text:"")
-            self.model.add_component(component: model_comp_text)
-            self.add_view_comp_text(model:model_comp_text)
-            
-            s_generic_index = s_generic_index + 1
+            s_generic_index += 1
             
             self.render()
         })
@@ -72,13 +81,20 @@ class Controller_Detail: UIViewController
         self.render()
     }
     
-    func add_view_comp_text(model:Model_Component_Text)
+    func add_view_comp_text(model: Model_Component_Text)
     {
-        var new_view_comp_text = View_Component_Text(model:model, ctrl_detail:self)
+        let new_view_comp_text = View_Component_Text(model: model, ctrl_detail: self)
         
         self.map_view.append(new_view_comp_text)
-        
         self.view.addSubview(new_view_comp_text.text_view)
+    }
+    
+    func add_view_comp_date(model: Model_Component_Date)
+    {
+        let new_view_comp_date = View_Component_Date(model: model, ctrl_detail: self)
+        
+        self.map_view.append(new_view_comp_date)
+        self.view.addSubview(new_view_comp_date.text_view)
     }
     
     func render()
