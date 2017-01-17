d6b6f8146bf1460a4070f44e9b81a6c8ba99b77b
wjdwithyou <asdf142857@gmail.com>
2017-01-17 23:19:24 +0900

    remove .d files

--- build/Pods.build/Debug-iphonesimulator/JTAppleCalendar.build/Objects-normal/x86_64/JTAppleCalendar-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/J.Style/Desktop/TheBigPicture/Pods/Target\ Support\ Files/JTAppleCalendar/JTAppleCalendar-dummy.m \
-  /Users/J.Style/Desktop/TheBigPicture/Pods/Target\ Support\ Files/JTAppleCalendar/JTAppleCalendar-prefix.pch
--- build/Pods.build/Debug-iphonesimulator/JTAppleCalendar.build/Objects-normal/x86_64/JTAppleCalendar_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/J.Style/Desktop/TheBigPicture/build/Pods.build/Debug-iphonesimulator/JTAppleCalendar.build/DerivedSources/JTAppleCalendar_vers.c
